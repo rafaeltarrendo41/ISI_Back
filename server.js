92fb36f24d1d640791723aa007b9a314e1996bfa
rafaeltarrendo41 <a89162@alunos.uminho.pt>
2021-04-12 16:25:37 +0100

    mini servidor

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const port = process.env.PORT || 3306;
+const host = process.env.HOST || '193.136.11.180';
 
+var app = express();
+
+app.listen(process.env.PORT, function(err){
+    if(!err){
+        console.log('Your app is listening on ' + host + ' and port ' + port);
+    } else {
+        console.log(err);
+    }
+});
