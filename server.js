7088f50b45652168e5856e209c56fa9c0ee6001d
rafaeltarrendo41 <a89162@alunos.uminho.pt>
2021-04-12 19:08:13 +0100

    dotenv

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const port = process.env.PORT || 3306;
 const host = process.env.HOST || '193.136.11.180';
+
+global.urlBase = 'https://wtransnet.herokuapp.com/';
 
 var app = express();
 
 const server = app.listen(process.env.PORT, function(err){
     if(!err){
-        console.log('Your app is listening on ' + host + ' and port ' + port);
+        console.log(`Your app is listening on ${global.urlBase}`);
     } else {
         console.log(err);
     }
