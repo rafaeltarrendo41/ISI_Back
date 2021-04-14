2743a174c2cdd0d1446690900b3c88ead3901b7c
alexandrefariag <alexandrefariag@gmail.com>
2021-04-14 22:49:50 +0100

    Update hubspot.controller.js

--- controllers/hubspot.controller.js
+++ controllers/hubspot.controller.js
@@ -28,8 +28,72 @@ function getCompanies(callback) {
     });
 }
 
+/*
+POST
+Função que permite criar clientes no Hubspot
+*/
 
+function postCompanies(callback) {
+    var options = {
+        method: 'POST',
+        url: 'https://api.hubapi.com/crm/v3/objects/companies',
+        qs: {
+            limit: '',
+            archived: 'false',
+            hapikey: process.env.HUBSPOT_KEY
+        },
+        headers: {
+            accept: 'application/json', 'content-type': 'application/json',
+            authorization: 'Bearer '+ process.env.HUBSPOT_KEY
+        },
+        body: {
+            //inserir atributos necessarios para a criaçao da empresa e ir buscar as variaveis
+            properties: {city: '', domain: '', industry: '', name: '', phone: '', state: '', VAT: ''}
+        },
+        json: true
+      };
+      
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+      
+        console.log(body);
+      });
+}
+
+/*
+PATCH
+Função que permite atualizar os clientes no Hubspot através do companyId
+*/
+
+function updateCompanies(callback) {
+    var options = {
+        method: 'PATCH',
+        url: 'https://api.hubapi.com/crm/v3/objects/companies/5860045128',
+        qs: {
+            limit: '',
+            archived: 'false',
+            hapikey: process.env.HUBSPOT_KEY
+        },
+        headers: {
+            accept: 'application/json', 'content-type': 'application/json',
+            authorization: 'Bearer '+ process.env.HUBSPOT_KEY
+        },
+        body: {
+            //inserir atributos necessarios para a criaçao da empresa e ir buscar as variaveis
+            properties: {city: '', domain: '', industry: '', name: '', phone: '', state: '', VAT: ''}
+        },
+        json: true
+      };
+      
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+      
+        console.log(body);
+      });
+}
 
 module.exports = {
-    getCompanies: getCompanies
+    getCompanies: getCompanies,
+    postCompanies: postCompanies,
+    updateCompanies:updateCompanies
 }
