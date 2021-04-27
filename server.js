5c92ddd9b04f34928b096adb4a5716391bad8737
rafaeltarrendo41 <a89162@alunos.uminho.pt>
2021-04-27 16:32:23 +0100

    aa

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const express = require('express');
-var app = express();
+const cors = require('cors');
+const app = express();
+app.use(cors());
 
 const bodyParser = require('body-parser');
 const validator = require('express-validator');
@@ -32,12 +34,12 @@ app.use(bodyParser.json({
 app.set("trust proxy", 1);
 
 //CORS
-app.use('/', function(request, response, next){
-    response.header('Access-Control-Allow-Origin', urlFront);
-    response.header('Access-Control-Allow-Creditials', true);
-    response.header('Access-Control-Allow-Methods', 'PUT, POST, OPTIONS, GET');
-    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-})
+// app.use('/', function(request, response, next){
+//     response.header('Access-Control-Allow-Origin', urlFront);
+//     response.header('Access-Control-Allow-Creditials', true);
+//     response.header('Access-Control-Allow-Methods', 'PUT, POST, OPTIONS, GET');
+//     response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// })
 
 
 //require('./routes/auth.routes')(app, passport);
